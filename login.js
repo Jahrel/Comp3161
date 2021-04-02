bb45a8bdc3fb68e99bb3058186eb43f6331b42e7
jahrel <jahr3lm@gmail.com>
2021-04-02 18:35:48 -0500

    Linkage between pages

---
+++ login.js
@@ -0,0 +1,10 @@
+window.onload = function (){
+
+    var login = document.getElementsByClassName("log");
+    login.addEventListener("click", () => {
+
+        window.location = "landing.html";
+
+    });
+
+}
