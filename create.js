1a3bac695568869d520fee52cc69f468e8c4550f
Jahrel <71864779+Jahrel@users.noreply.github.com>
2021-04-03 22:59:45 -0500

    Add files via upload

---
+++ create.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+
+    var subbtn = document.getElementsByClassName("btn")[0];
+    const myForm = document.getElementById("myForm");
+   
+    myForm.addEventListener("submit", () => {
+
+    const formData = new FormData(this):
+    
+
+
+    });
+
+
+
+
+}
